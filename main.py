29a60e1d639673bd317bbc8df0af765ffb18f7d8
keikyoku <boboul.benjamin@outlook.fr>
2016-03-08 14:55:10 +0100

    TEST

---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+import xbeeInit, processData, var
+import os, sys, time, fcntl, serial
+
+if __name__ == '__main__':
+	print var.temp
---
+++ var.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+temp = 0.0
+lum = 0.0
+serial = serialSerial()
