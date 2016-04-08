c3c973ae71c15eda66a853106fe3f3f120442f39
Benjamin <boboul.benjamin@outlook.fr>
2016-04-08 18:30:29 +0200

    basic setup

---
+++ sas/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def core():
+	print("Script is ok.")
+	pass
---
+++ setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sas
+print('launching sasScript... please wait.')
+
+if __name__ == '__main__':
+	sas.core()
